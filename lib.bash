eae789eb50c84835ff943d8c69c9a668255f79da
Rik Tytgat <rik.tytgat@tryx.com>
2018-04-09 09:58:15 +0200

    Add logging

--- lib.bash
+++ lib.bash
@@ -7,12 +7,12 @@ install_awscli() {
 }
 
 set_source_ecr_credentials() {
-  echo "### Setting environment for AWS authentication ###"
+  echo "### Setting environment for AWS authentication for ECR source ###"
   AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID_ECR_SOURCE}"
   AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY_ECR_SOURCE}"
   export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY
-  echo "### Logging in to AWS ECR ###"
+  echo "### Logging in to AWS ECR source ###"
   eval $(aws ecr get-login --no-include-email --region ${AWS_REGION_SOURCE:-eu-central-1})
 }
 
@@ -24,18 +24,22 @@ docker_build_deploy_image() {
 }
 
 set_dest_ecr_credentials() {
+  echo "### Setting environment for AWS authentication for ECR target###"
   AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID_ECR_TARGET}"
   AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY_ECR_TARGET}"
   export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY
+  echo "### Logging in to AWS ECR target ###"
   eval $(aws ecr get-login --no-include-email --region ${AWS_REGION_TARGET:-eu-central-1})
 }
 
 docker_tag_and_push_deploy_image() {
+  echo "### Tagging docker image ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} ###"
   docker tag ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${DOCKER_IMAGE}-${ENVIRONMENT:-dev}
+  echo "### Pushing docker image ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} to ECR ###"
   docker push ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${DOCKER_IMAGE}-${ENVIRONMENT:-dev}
 }
 
 docker_deploy_image() {
-  echo "Not yet implemented!!"
+  echo "### Not yet implemented!! ###"
 }
