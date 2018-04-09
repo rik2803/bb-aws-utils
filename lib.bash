47493a711f8c802a1d48f1aab7ff3f264cc67ad8
Rik Tytgat <rik.tytgat@tryx.com>
2018-04-09 09:55:27 +0200

    Changed REPONAME to DOCKER_IMAGE

--- lib.bash
+++ lib.bash
@@ -7,16 +7,20 @@ install_awscli() {
 }
 
 set_source_ecr_credentials() {
+  echo "### Setting environment for AWS authentication ###"
   AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID_ECR_SOURCE}"
   AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY_ECR_SOURCE}"
   export AWS_ACCESS_KEY_ID
   export AWS_SECRET_ACCESS_KEY
+  echo "### Logging in to AWS ECR ###"
   eval $(aws ecr get-login --no-include-email --region ${AWS_REGION_SOURCE:-eu-central-1})
 }
 
 docker_build_deploy_image() {
-  echo "FROM ${AWS_ACCOUNTID_SRC}.dkr.ecr.${AWS_REGION_SOURCE:-eu-central-1}.amazonaws.com/${REPONAME}:latest" > Dockerfile
-  docker build -t ${REPONAME}-${ENVIRONMENT:-dev} .
+  echo "### Create Dockerfile ###"
+  echo "FROM ${AWS_ACCOUNTID_SRC}.dkr.ecr.${AWS_REGION_SOURCE:-eu-central-1}.amazonaws.com/${DOCKER_IMAGE}:latest" > Dockerfile
+  echo "### Start build og docker image ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} ###"
+  docker build -t ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} .
 }
 
 set_dest_ecr_credentials() {
@@ -28,8 +32,8 @@ set_dest_ecr_credentials() {
 }
 
 docker_tag_and_push_deploy_image() {
-  docker tag ${REPONAME}-${ENVIRONMENT:-dev} ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${REPONAME}-${ENVIRONMENT:-dev}
-  docker push ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${REPONAME}-${ENVIRONMENT:-dev}
+  docker tag ${DOCKER_IMAGE}-${ENVIRONMENT:-dev} ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${DOCKER_IMAGE}-${ENVIRONMENT:-dev}
+  docker push ${AWS_ACCOUNTID_TARGET}.dkr.ecr.${AWS_REGION_TARGET:-eu-central-1}.amazonaws.com/${DOCKER_IMAGE}-${ENVIRONMENT:-dev}
 }
 
 docker_deploy_image() {
