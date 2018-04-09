513e5effa1646967920f3c1709e6a9bf119c62db
Rik Tytgat <rik.tytgat@tryx.com>
2018-04-09 13:58:47 +0200

    Add script to start pipeline of BB repo REMOTE_REPO_SLUG, wait for it to finish and return the correct result

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Pipeline requirements
 
-### Environment
+### Environment for docker image build and push
 
 * `AWS_ACCESS_KEY_ID_ECR_SOURCE`
 * `AWS_SECRET_ACCESS_KEY_ECR_SOURCE`
@@ -11,3 +11,13 @@
 * `AWS_SECRET_ACCESS_KEY_ECR_TARGET`
 * `AWS_REGION_TARGET`: Optional, default is `eu-central-1`
 * `DOCKER_IMAGE`
+
+### Environment for triggering other pipeline
+
+IMPORTANT: The script `sync_trigger_bb_build.bash` requires `jq`
+
+* `BB_USER`
+* `BB_APP_PASSWORD`: https://confluence.atlassian.com/bitbucket/app-passwords-828781300.html
+* `REMOTE_REPO_OWNER`: Defaults to `${BITBUCKET_REPO_OWNER}`. This is an environment
+  variable made available by BB.
+* `REMOTE_REPO_SLUG`
---
+++ sync_trigger_bb_build.bash
@@ -0,0 +1,49 @@
+#! /bin/bash
+
+### jq is required
+if ! which jq >/dev/null 2>&1
+then
+  echo "### jq is required ###"
+  exit 1
+fi
+ 
+[[ -z ${REMOTE_REPO_SLUG} ]] && { echo "REMOTE_REPO_SLUG is required"; exit 1; }
+[[ -z ${BB_USER} ]]          && { echo "BB_USER is required"; exit 1; }
+[[ -z ${BB_APP_PASSWORD} ]]  && { echo "BB_APP_PASSWORD is required"; exit 1; }
+
+REPO_OWNER=${REMOTE_REPO_OWNER:=BITBUCKET_REPO_OWNER}
+REPO_SLUG=${REMOTE_REPO_SLUG:=THIS_ONE_IS_REQUIRED}
+
+export URL="https://api.bitbucket.org/2.0/repositories/${REPO_OWNER}/${REPO_SLUG}/pipelines/"
+
+CURLRESULT=$(curl -X POST -s -u "${BB_USER}:${BB_APP_PASSWORD}" -H 'Content-Type: application/json' \
+                  ${URL} -d '{ "target": { "ref_type": "branch", "type": "pipeline_ref_target", "ref_name": "master" } }')
+
+UUID=$(echo "${CURLRESULT}" | jq --raw-output '.uuid' | tr -d '\{\}')
+
+echo "UUID is ${UUID}"
+
+CONTINUE=1
+SLEEP=10
+STATE="NA"
+RESULT="na"
+CURLRESULT="NA"
+
+while [[ ${CONTINUE} = 1 ]]
+do
+  sleep ${SLEEP}
+  CURLRESULT=$(curl -X GET -s -u "${BB_USER}:${BB_APP_PASSWORD}" -H 'Content-Type: application/json' ${URL}\\{${UUID}\\})
+  STATE=$(echo ${CURLRESULT} | jq --raw-output '.state.name')
+  
+  echo " ### Pipeline is in state ${STATE} ###"
+
+  if [[ ${STATE} == "COMPLETED" ]]
+  then
+    CONTINUE=0
+  fi
+done
+
+RESULT=$(echo ${CURLRESULT} | jq --raw-output '.state.result.name')
+echo " ### Pipeline result is ${RESULT} ###"
+[[ ${RESULT} == "SUCCESSFUL" ]] && exit 0
+exit 1
