a4c51aeb182ca1a2f3dce5fe54f6e0a5c4ceaeb7
Rik Tytgat <rik.tytgat@tryx.com>
2018-05-07 07:09:54 +0200

    Add logging

--- lib.bash
+++ lib.bash
@@ -67,33 +67,46 @@ s3_deploy_apply_config_to_tree() {
 }
 
 s3_deploy_create_tar_and_upload_to_s3() {
+  echo "### Ccreate tarfile ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz from all files in ${PAYLOAD_LOCATION:-dist} ###"
   tar -C ${PAYLOAD_LOCATION:-dist} -czvf ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz .
+  echo "### Copy ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz to S3 bucket ${S3_ARTIFACT_BUCKET}/${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz ###"
   aws s3 cp ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz s3://${S3_ARTIFACT_BUCKET}/${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz
+  echo "### Copy ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz to S3 bucket ${S3_ARTIFACT_BUCKET}/${ARTIFACT_NAME}-last.tgz ###"
   aws s3 cp ${ARTIFACT_NAME}-${BITBUCKET_COMMIT}.tgz s3://${S3_ARTIFACT_BUCKET}/${ARTIFACT_NAME}-last.tgz
 }
 
 s3_deploy_download_tar_and_prepare_for_deploy() {
+  echo "### Download artifact ${ARTIFACT_NAME}-last.tgz from s3://${S3_ARTIFACT_BUCKET} ###"
   aws s3 cp s3://${S3_ARTIFACT_BUCKET}/${ARTIFACT_NAME}-last.tgz .
+  echo "### Create workdir ###"
   mkdir -p workdir
+  echo "### Untar the artifact file into the workdir ###"
   tar -C workdir -xzvf ${ARTIFACT_NAME}-last.tgz
+  echo "### Start applying the config to the untarred files ###"
   s3_deploy_apply_config_to_tree workdir
 }
 
 s3_deploy_deploy() {
   cd workdir
+  echo "### Deploy the payload to s3://${S3_DEST_BUCKET}/${S3_PREFIX} with ACL ${AWS_ACCESS_CONTROL:-private} ###"
   aws s3 cp --acl ${AWS_ACCESS_CONTROL:-private} --recursive . s3://${S3_DEST_BUCKET}/${S3_PREFIX}
   cd -
 }
 
 s3_deploy() {
+  echo "### Set AWS credentials for artifact download (AWS_ACCESS_KEY_ID_S3_SOURCE and AWS_SECRET_ACCESS_KEY_S3_SOURCE) ###"
   set_credentials "${AWS_ACCESS_KEY_ID_S3_SOURCE}" "${AWS_SECRET_ACCESS_KEY_S3_SOURCE}"
   s3_deploy_download_tar_and_prepare_for_deploy
+  echo "### Set AWS credentials for deploy (AWS_ACCESS_KEY_ID_S3_TARGET and AWS_SECRET_ACCESS_KEY_S3_TARGET) ###"
   set_credentials "${AWS_ACCESS_KEY_ID_S3_TARGET}" "${AWS_SECRET_ACCESS_KEY_S3_TARGET}"
+  echo "### Start the deploy ###"
   s3_deploy_deploy
 }
 
 s3_artifact() {
+  echo "### Run the build command (${BUILD_COMMAND:-No build command}) ###"
   [[ -n ${BUILD_COMMAND} ]] && eval ${BUILD_COMMAND}
+  echo "### Set AWS credentials for artifact upload (AWS_ACCESS_KEY_ID_S3_TARGET and AWS_SECRET_ACCESS_KEY_S3_TARGET) ###"
   set_credentials "${AWS_ACCESS_KEY_ID_S3_TARGET}" "${AWS_SECRET_ACCESS_KEY_S3_TARGET}"
   s3_deploy_create_tar_and_upload_to_s3
 }
