5bbedf31db842429ab655c0ab3b29ca25c12a160
Rik Tytgat <rik.tytgat@tryx.com>
2018-05-16 13:57:50 +0200

    Create TAG file in remote repo to allow it to pull the correct docker image

--- sync_trigger_bb_build.bash
+++ sync_trigger_bb_build.bash
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 echo "### Starting $(basename "${0}") ###"
-### Install jq
+
 scriptdir=$(dirname "${0}")
 source ${scriptdir}/lib.bash
 
@@ -21,6 +21,18 @@ fi
 [[ -z ${REMOTE_REPO_OWNER} ]] && { echo "REMOTE_REPO_OWNER is required"; exit 1; }
 [[ -z ${BB_USER} ]]           && { echo "BB_USER is required"; exit 1; }
 [[ -z ${BB_APP_PASSWORD} ]]   && { echo "BB_APP_PASSWORD is required"; exit 1; }
+
+### Construct remote repo HTTPS URL
+REMOTE_REPO_URL="https://${BB_USER}:${BB_APP_PASSWORD}@bitbucket.org/${REMOTE_REPO_OWNER}/${REMOTE_REPO_OWNER}"
+
+echo "### Trying to clone ${REMOTE_REPO_URL} into remote_repo ###"
+git clone ${REMOTE_REPO_URL} remote_repo || { echo "### Error cloning ${REMOTE_REPO_URL} ###"; exit 1; }
+echo "### Update the TAG file in the repor ###"
+echo "${BITBUCKET_COMMIT}" > remote_repo/TAG
+cd remote_repo
+git commit -m 'Update TAG with source repo commit hash' TAG || { echo "### Error committing TAG ###"; exit 1; }
+git push || { echo "### Error pushing to ${REMOTE_REPO_URL} ###"; exit 1; }
+cd -
 
 export URL="https://api.bitbucket.org/2.0/repositories/${REMOTE_REPO_OWNER}/${REMOTE_REPO_SLUG}/pipelines/"
 
