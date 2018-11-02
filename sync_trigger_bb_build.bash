09a296c957c96700d8d1189bf7f10366a7db4e42
Rik Tytgat <rik.tytgat@tryx.com>
2018-11-02 10:16:21 +0100

    Add logging

--- sync_trigger_bb_build.bash
+++ sync_trigger_bb_build.bash
@@ -14,11 +14,16 @@ install_jq
 
 create_TAG_file_in_remote_url
 
+echo "### sync_trigger_bb_build.bash - ONLY_MONITOR_REMOTE_PIPELINE is ${ONLY_MONITOR_REMOTE_PIPELINE:-Not Defined} ###"
 if [[ -n ${ONLY_MONITOR_REMOTE_PIPELINE} ]] && [[ ${ONLY_MONITOR_REMOTE_PIPELINE} -eq 1 ]]
 then
+  echo "### sync_trigger_bb_build.bash - Starting monitor_automatic_remote_pipeline_start .... ###"
   monitor_automatic_remote_pipeline_start
+  echo "### sync_trigger_bb_build.bash - monitor_automatic_remote_pipeline_start finished ###"
 else
+  echo "### sync_trigger_bb_build.bash - Starting start_pipeline_for_remote_repo .... ###"
   start_pipeline_for_remote_repo ${REMOTE_REPO_COMMIT_HASH} ${2:-build_and_deploy}
+  echo "### sync_trigger_bb_build.bash - start_pipeline_for_remote_repo finished ###"
 fi
 
 [[ ${RETURNVALUE} == "SUCCESSFUL" ]] && exit 0
