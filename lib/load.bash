4d3e79b94cb876ed5111930af3a836d5561c0289
Rik Tytgat <rik.tytgat@tryx.com>
2019-05-24 10:20:35 +0200

    Catch errors in load.bash

--- lib/load.bash
+++ lib/load.bash
@@ -1,6 +1,6 @@
-[[ -z ${LIB_COMMON_LOADED} ]]    && { source ${LIB_DIR:-lib}/common.bash; }
-[[ -z ${LIB_AWS_LOADED} ]]       && { source ${LIB_DIR:-lib}/aws.bash; }
-[[ -z ${LIB_DOCKERHIB_LOADED} ]] && { source ${LIB_DIR:-lib}/dockerhub.bash; }
-[[ -z ${LIB_GIT_LOADED} ]]       && { source ${LIB_DIR:-lib}/git.bash; }
-[[ -z ${LIB_MAVEN_LOADED} ]]     && { source ${LIB_DIR:-lib}/maven.bash; }
-[[ -z ${LIB_INSTALL_LOADED} ]]   && { source ${LIB_DIR:-lib}/install.bash; }
+[[ -z ${LIB_COMMON_LOADED} ]]    && source ${LIB_DIR:-lib}/common.bash    || true
+[[ -z ${LIB_AWS_LOADED} ]]       && source ${LIB_DIR:-lib}/aws.bash       || true
+[[ -z ${LIB_DOCKERHIB_LOADED} ]] && source ${LIB_DIR:-lib}/dockerhub.bash || true
+[[ -z ${LIB_GIT_LOADED} ]]       && source ${LIB_DIR:-lib}/git.bash       || true
+[[ -z ${LIB_MAVEN_LOADED} ]]     && source ${LIB_DIR:-lib}/maven.bash     || true
+[[ -z ${LIB_INSTALL_LOADED} ]]   && source ${LIB_DIR:-lib}/install.bash   || true
