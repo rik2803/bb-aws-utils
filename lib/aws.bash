9fb93c7172b767d3332458571c2be4192ec4bfe1
Rik Tytgat <rik.tytgat@tryx.com>
2019-05-30 20:08:21 +0200

    AccountId not required

--- lib/aws.bash
+++ lib/aws.bash
@@ -6,12 +6,11 @@ check_envvar AWS_DEFAULT_REGION O eu-central-1
 aws_update_service() {
   check_envvar AWS_DEFAULT_REGION R
   [[ -z ${1} || -z ${2} || -z ${3} || -z ${4} || -z ${5} ]] && \
-    fail "aws_update_service aws_account_id aws_ecs_cluster_name aws_ecs_service_name aws_ecs_task_family_name image_tag"
-  local aws_account_id=${1}
-  local aws_ecs_cluster_name=${2}
-  local aws_ecs_service_name=${3}
-  local aws_ecs_task_family_name=${4}
-  local image_tag=${5}
+    fail "aws_update_service aws_ecs_cluster_name aws_ecs_service_name aws_ecs_task_family_name image_tag"
+  local aws_ecs_cluster_name=${1}; shift
+  local aws_ecs_service_name=${1}; shift
+  local aws_ecs_task_family_name=${1}; shift
+  local image_tag=${1}; shift
 
   info "Creating task definition file for ${aws_ecs_task_family_name} with version ${image_tag}"
   aws_ecs_create_task_definition_file "${aws_ecs_task_family_name}:${image_tag}"
@@ -21,12 +20,12 @@ aws_update_service() {
   aws_ecs_register_taskdefinition "${aws_ecs_task_family_name}"
   success "Task definition successfully registgered"
 
-  info "Update service ${3} in cluster ${2} on AWS account ${1}"
+  info "Update service ${aws_ecs_service_name} in cluster ${aws_ecs_cluster_name}"
   aws ecs update-service --cluster ${aws_ecs_cluster_name} \
                          --task-definition ${AWS_ECS_NEW_TASK_DEFINITION_ARN} \
                          --force-new-deployment \
                          --service ${aws_ecs_service_name}
-  success "Successfully updated service ${3} in cluster ${2} on AWS account ${1}"
+  success "Successfully updated service ${aws_ecs_service_name} in cluster ${aws_ecs_cluster_name}"
 }
 
 
