d236a1c3b064d02de5382bc77cd88155084b29b0
Rik Tytgat <rik.tytgat@tryx.com>
2019-05-31 12:18:28 +0200

    Factor out use of AWS_ECS_TASK_FAMILY

--- lib/aws.bash
+++ lib/aws.bash
@@ -6,19 +6,19 @@ check_envvar AWS_DEFAULT_REGION O eu-central-1
 aws_update_service() {
   check_envvar AWS_DEFAULT_REGION R
   [[ -z ${1} || -z ${2} || -z ${3} || -z ${4} || -z ${5} ]] && \
-    fail "aws_update_service aws_ecs_cluster_name aws_ecs_service_name aws_ecs_task_family_name image_tag image_basename"
+    fail "aws_update_service aws_ecs_cluster_name aws_ecs_service_name aws_ecs_task_family image_tag image_basename"
   local aws_ecs_cluster_name=${1}; shift
   local aws_ecs_service_name=${1}; shift
-  local aws_ecs_task_family_name=${1}; shift
+  local aws_ecs_task_family=${1}; shift
   local image_tag=${1}; shift
   local image_basename=${1}; shift
 
-  info "Creating task definition file for ${aws_ecs_task_family_name} with version ${image_tag}"
-  aws_ecs_create_task_definition_file "${image_basename}:${image_tag}"
+  info "Creating task definition file for ${aws_ecs_task_family} with version ${image_tag}"
+  aws_ecs_create_task_definition_file ${aws_ecs_task_family} "${image_basename}:${image_tag}"
   success "Task definition file successfully created"
 
-  info "Registering task definition file for ${aws_ecs_task_family_name} with version ${image_tag}"
-  aws_ecs_register_taskdefinition "${aws_ecs_task_family_name}"
+  info "Registering task definition file for ${aws_ecs_task_family} with version ${image_tag}"
+  aws_ecs_register_taskdefinition "${aws_ecs_task_family}"
   success "Task definition successfully registgered"
 
   info "Update service ${aws_ecs_service_name} in cluster ${aws_ecs_cluster_name}"
@@ -46,11 +46,11 @@ aws_update_service() {
 aws_ecs_create_task_definition_file() {
   check_command aws || install_awscli
   check_command jq || install_sw jq
-  check_envvar AWS_ECS_TASK_FAMILY R
-  [[ -z ${1} ]] && fail "aws_ecs_create_task_definition_file docker_image"
+  [[ -z ${1} || -z ${2} ]] && fail "aws_ecs_create_task_definition_file aws_ecs_task_family docker_image"
   local aws_image=${1}; shift
+  local aws_ecs_task_family=${1}; shift
 
-  aws ecs describe-task-definition --task-definition ${AWS_ECS_TASK_FAMILY} \
+  aws ecs describe-task-definition --task-definition ${aws_ecs_task_family} \
                                    --query 'taskDefinition' | \
                                    jq "del(.taskDefinitionArn, .revision, .status, .requiresAttributes, .compatibilities)" | \
                                    jq ".containerDefinitions[0].image = \"${aws_image}\"" > /taskdefinition.json
@@ -66,11 +66,12 @@ aws_ecs_register_taskdefinition() {
   # Limitation: only supports task definitions with 1 containerDefinition
   check_command aws || install_awscli
   check_command jq || install_sw jq
-  check_envvar AWS_ECS_TASK_FAMILY R
+  [[ -z ${1} ]] && fail "aws_ecs_create_task_definition_file aws_ecs_task_family docker_image"
+  local aws_ecs_task_family=${1}; shift
 
-  info "Registering a new task definition for ${AWS_ECS_TASK_FAMILY}"
-  RESULT=$(aws ecs register-task-definition --family ${AWS_ECS_TASK_FAMILY} --cli-input-json file:///taskdefinition.json)
+  info "Registering a new task definition for ${aws_ecs_task_family}"
+  RESULT=$(aws ecs register-task-definition --family ${aws_ecs_task_family} --cli-input-json file:///taskdefinition.json)
   AWS_ECS_NEW_TASK_DEFINITION_ARN=$(echo ${RESULT} | jq -r '.taskDefinition.taskDefinitionArn')
-  success "Successfully registered new task definition for ${AWS_ECS_TASK_FAMILY}"
+  success "Successfully registered new task definition for ${aws_ecs_task_family}"
   info "New task definition ARN is ${AWS_ECS_NEW_TASK_DEFINITION_ARN}"
 }
