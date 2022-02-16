7e2b496721e1ecd59ccb4fc30d52bc2dc855fa40
Rik Tytgat <rik.tytgat@tryx.com>
2022-02-16 07:52:15 +0100

    improvement: datadog_deploy_monitors: Use ENVIRONMENT and BITBUCKET_DEPLOYMENT_ENVIRONMENT to offer the possibility to avoid deploying the monitors for all environments at the same time.

--- lib/datadog.bash
+++ lib/datadog.bash
@@ -4,11 +4,16 @@ export LIB_DATADOG_LOADED=1
 
 datadog_deploy_monitors() {
   local docker_image="ixor/ansible-datadog-monitor:latest"
+  local environment="${1:-all}"
+
   info "datadog: Check for ${BITBUCKET_CLONE_DIR}/dd_monitors.yml"
+
   if [[ ! -e "${BITBUCKET_CLONE_DIR}/dd_monitors.yml" ]]; then
     info "datadog: ${BITBUCKET_CLONE_DIR}/dd_monitors.yml not found, will not create/update DD monitors"
     return 0
   fi
+
+  info "datadog: ${BITBUCKET_CLONE_DIR}/dd_monitors.yml found, continuing."
 
   if [[ -n "${DATADOG_MONITOR_SKIP_AUTO_RUN}" && "${DATADOG_MONITOR_SKIP_AUTO_RUN}" -eq 1 ]]; then
     info "datadog: Will not automatically create/update DD monitors because DATADOG_MONITOR_SKIP_AUTO_RUN == 1"
@@ -23,6 +28,8 @@ datadog_deploy_monitors() {
   docker run \
     -e DD_API_KEY="${DD_API_KEY}" \
     -e DD_APP_KEY="${DD_APP_KEY}" \
+    -e ENVIRONMENT="${environment}" \
+    -e BITBUCKET_DEPLOYMENT_ENVIRONMENT="${BITBUCKET_DEPLOYMENT_ENVIRONMENT:-all}" \
     -v ${BITBUCKET_CLONE_DIR}/dd_monitors.yml:/ansible/dd_monitors.yml \
     -v ${BITBUCKET_CLONE_DIR}/bb-aws-utils/ansible_datadog/playbook.yml:/ansible/playbook.yml \
     -v ${BITBUCKET_CLONE_DIR}/bb-aws-utils/ansible_datadog/datadog_monitors_template.j2:/ansible/datadog_monitors_template.j2 \
