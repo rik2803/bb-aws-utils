ef264fdc52aca5072db1c4e71ff41dce530e5028
Rik Tytgat <rik.tytgat@tryx.com>
2022-06-09 11:55:55 +0200

    improvement(snyk): Add docker test function snyk_run_docker_test

--- lib/snyk.bash
+++ lib/snyk.bash
@@ -2,17 +2,40 @@
 [[ -z ${LIB_COMMON_LOADED} ]] && { source "${LIB_DIR:-lib}/common.bash"; }
 export LIB_SNYK_LOADED=1
 
-snyk_run_test() {
+_snyk_prerun_checks() {
+  [[ -z "${SNYK_TOKEN}" ]] && { info "SNYK_TOKEN not set."; return 1; }
+  [[ -n "${SNYK_SKIP_TEST}" && ${SNYK_SKIP_TEST} -eq 1  ]] && { warning "SNYK_SKIP_TEST is set and 1, skipping sny test (NOT GOOD!!)."; return 1; }
+}
+
+_snyk_install_snyk() {
   local snyk_cli_url="https://static.snyk.io/cli/latest/snyk-linux"
 
-  [[ -z "${SNYK_TOKEN}" ]] && { info "SNYK_TOKEN not set."; return; }
-  [[ -n "${SNYK_SKIP_TEST}" && ${SNYK_SKIP_TEST} -eq 1  ]] && { warning "SNYK_SKIP_TEST is set and 1, skipping sny test (NOT GOOD!!)."; return; }
+  if [[ -x /snyk ]]; then
+    info "Snyk CLI already installed."
+  else
+    check_command curl || install_sw curl
+    info "snyk: Download Snyk CLI ..."
+    run_cmd curl -s "${snyk_cli_url}" -o /snyk
+    run_cmd chmod 0755 /snyk
+  fi
+}
 
-  check_command curl || install_sw curl
+snyk_run_test() {
+  _snyk_prerun_checks || return
   info "snyk: SNYK_TOKEN is set, starting Snyk analysis."
-  info "snyk: Download Snyk CLI ..."
-  run_cmd curl -s "${snyk_cli_url}" -o /snyk
-  run_cmd chmod 0755 /snyk
-  info "snyk: Run snyk test"
+  _snyk_install_snyk
   /snyk test --severity-threshold="${SNYK_SEVERITY_THRESHOLD:-high}" --all-projects
+}
+
+snyk_run_docker_test() {
+  # This requires that the image be already available
+  _snyk_prerun_checks || return
+
+  check_envvar DOCKER_IMAGE R
+
+  info "snyk: SNYK_TOKEN is set, starting Snyk container analysis."
+  _snyk_install_snyk
+  /snyk container test "${DOCKER_IMAGE}" \
+    --file=src/main/docker/Dockerfile \
+    --severity-threshold="${SNYK_SEVERITY_THRESHOLD:-high}"
 }
