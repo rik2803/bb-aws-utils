600411b55babc3f99e2ee53fd31afda51270caca
Rik Tytgat <rik.tytgat@tryx.com>
2022-06-30 17:27:18 +0200

    fix(datadog): BB only allows files under BITBUCKET_CLONE_DIR to be used in docker volume arguments, copy the relevant files to ${BITBUCKET_CLONE_DIR}/bb-aws-utils-datadog-tmp to work around that restriction

--- lib/datadog.bash
+++ lib/datadog.bash
@@ -9,14 +9,14 @@ datadog_deploy_monitors() {
   #   DATADOG_MONITOR_AUTO_RUN not set or DATADOG_MONITOR_AUTO_RUN == 0
   #   DATADOG_MONITOR_ENVIRONMENT not set
   #   BITBUCKET_DEPLOYMENT_ENVIRONMENT not set
-  if [[ -z "${DATADOG_MONITOR_AUTO_RUN}" || "${DATADOG_MONITOR_AUTO_RUN}" -eq 0 ]] && \
-     [[ -z "${DATADOG_MONITOR_ENVIRONMENT}" ]] &&
-     [[ -z "${BITBUCKET_DEPLOYMENT_ENVIRONMENT}" ]]; then
-     info "datadog monitors: Monitors will not be deployed because:"
-     info "                  DATADOG_MONITOR_AUTO_RUN is not set or 0 AND"
-     info "                  DATADOG_MONITOR_ENVIRONMENT is in the pipleine environment 0 AND"
-     info "                  This pipeline is not a BB deployment"
-     return 0
+  if [[ -z "${DATADOG_MONITOR_AUTO_RUN}" || "${DATADOG_MONITOR_AUTO_RUN}" -eq 0 ]] &&
+    [[ -z "${DATADOG_MONITOR_ENVIRONMENT}" ]] &&
+    [[ -z "${BITBUCKET_DEPLOYMENT_ENVIRONMENT}" ]]; then
+    info "datadog monitors: Monitors will not be deployed because:"
+    info "                  DATADOG_MONITOR_AUTO_RUN is not set or 0 AND"
+    info "                  DATADOG_MONITOR_ENVIRONMENT is in the pipleine environment 0 AND"
+    info "                  This pipeline is not a BB deployment"
+    return 0
   fi
 
   info "datadog monitors: Check for ${BITBUCKET_CLONE_DIR}/dd_monitors.yml"
@@ -40,13 +40,17 @@ datadog_deploy_monitors() {
   check_envvar DD_API_KEY R
   check_envvar DD_APP_KEY R
 
-  ANSIBLE_PLAYBOOK_SOURCE_DIR="${BITBUCKET_CLONE_DIR}/bb-aws-utils"
-  if [[ -n ${LIB_DIR} ]]; then
-    info "Look for Ansible files in ${LIB_DIR}/../ansible_datadog"
-    ANSIBLE_PLAYBOOK_SOURCE_DIR="${LIB_DIR}/../ansible_datadog"
-  else
-    info "Look for Ansible files in ${BITBUCKET_CLONE_DIR}/bb-aws-utils"
-  fi
+  # When bb-aws-utils is cloned outside BITBUCKET_CLONE_DIR, the files ${LIB_DIR}/ansible_datadog/playbook.yml and
+  # ${LIB_DIR}/ansible_datadog/datadog_monitors_template.j2 should be copied somewhere under BITBUCKET_CLONE_DIR because
+  # BB pipelines only accepts files under BITBUCKET_CLONE_DIR to be passed in the docker volume mount arguments. The
+  # error produced otherwise by the pipeline is:
+  #
+  # docker: Error response from daemon: authorization denied by plugin pipelines: -v only supports $BITBUCKET_CLONE_DIR and its subdirectories.
+  #    See 'docker run --help'.
+
+  mkdir -p ${BITBUCKET_CLONE_DIR}/bb-aws-utils-datadog-tmp
+  cp ${LIB_DIR}/../ansible_datadog/* ${BITBUCKET_CLONE_DIR}/bb-aws-utils-datadog-tmp
+  ANSIBLE_PLAYBOOK_SOURCE_DIR="${BITBUCKET_CLONE_DIR}/bb-aws-utils-datadog-tmp"
 
   docker pull -q "${docker_image}"
   docker run \
