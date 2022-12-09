fbd35d6f29e5b26e304ba74530eaf31afd9cf313
Rik Tytgat <rik.tytgat@tryx.com>
2022-12-09 13:06:15 +0100

    snyk improvements

--- lib/snyk.bash
+++ lib/snyk.bash
@@ -1,10 +1,11 @@
 # shellcheck source=../../bb-aws-utils/lib/common.bash
 [[ -z ${LIB_COMMON_LOADED} ]] && { source "${LIB_DIR:-lib}/common.bash"; }
 export LIB_SNYK_LOADED=1
+export LIB_SNYK_TEST_ALREADY_PERFORMED=0
 
 _snyk_prerun_checks() {
   [[ -z "${SNYK_TOKEN}" ]] && { info "SNYK_TOKEN not set."; return 1; }
-  [[ -n "${SNYK_SKIP_TEST}" && ${SNYK_SKIP_TEST} -eq 1  ]] && { warning "SNYK_SKIP_TEST is set and 1, skipping sny test (NOT GOOD!!)."; return 1; }
+  [[ -n "${SNYK_SKIP_TEST}" && ${SNYK_SKIP_TEST} -eq 1  ]] && { warning "SNYK_SKIP_TEST is set and 1, skipping snyk test (NOT GOOD!!)."; return 1; }
   return 0
 }
 
@@ -24,8 +25,16 @@ _snyk_install_snyk() {
 snyk_run_test() {
   _snyk_prerun_checks || return
   info "snyk: SNYK_TOKEN is set, starting Snyk analysis."
-  _snyk_install_snyk
+  if [[ ${LIB_SNYK_TEST_ALREADY_PERFORMED} -ne 0 ]]; then
+    info "snyk: snyk test was already run in this pipeline, skipping this run."
+  fi
+
+   _snyk_install_snyk
+  info "snyk: Run snyk monitor to register project with Snyk back-end"
+  /snyk monitor --all-projects
+  info "snyk: Run snyk test to check dependencies"
   /snyk test --severity-threshold="${SNYK_SEVERITY_THRESHOLD:-high}" --all-projects
+  LIB_SNYK_TEST_ALREADY_PERFORMED=1
 }
 
 snyk_run_docker_test() {
